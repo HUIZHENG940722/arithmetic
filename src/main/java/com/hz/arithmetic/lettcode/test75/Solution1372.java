2303b074d78cf1a43bca0dd1c37a1f5d977e041e
zhenghui <15270855286@163.com>
2023-10-25 16:51:10 +0800

    20231023

--- src/main/java/com/hz/arithmetic/lettcode/test75/Solution1372.java
+++ src/main/java/com/hz/arithmetic/lettcode/test75/Solution1372.java
@@ -9,47 +9,6 @@ package com.hz.arithmetic.lettcode.test75;
  */
 public class Solution1372 {
 
-  public int longestZigZag(TreeNode root) {
-    int max = 0;
-    if (root == null) {
-      return 0;
-    } else if (root.left == null && root.right == null) {
-      return 0;
-    }
-    max = Math.max(max, longestZigZag(root.left));
-    max = Math.max(max, longestZigZag(root.right));
-    return max;
-  }
-
-  private int zigZag(TreeNode node) {
-    int flag = 0;
-    TreeNode p = node;
-    int res1 = 0, res2 = 0;
-    while (p != null) {
-      res1++;
-      if (flag == 0) {
-        flag = 1;
-        p = p.left;
-      } else {
-        flag = 0;
-        p = p.right;
-      }
-    }
-    p = node;
-    flag = 1;
-    while (p != null) {
-      res2++;
-      if (flag == 0) {
-        flag = 1;
-        p = p.left;
-      } else {
-        flag = 0;
-        p = p.right;
-      }
-    }
-    return Math.max(res1, res2) - 1;
-  }
-
   private static class TreeNode {
 
     int val;
@@ -67,6 +26,78 @@ public class Solution1372 {
       this.val = val;
       this.left = left;
       this.right = right;
+    }
+  }
+
+  public int longestZigZag(TreeNode root) {
+    int[] result = new int[]{0};
+    longestZigZag(root, result);
+    return result[0];
+  }
+
+  /**
+   * 先序遍历
+   *
+   * @param node
+   * @param result
+   */
+  private void longestZigZag(TreeNode node, int[] result) {
+    if (node != null) {
+      result[0] = Math.max(result[0],
+          Math.max(longestZigZag(node, true), longestZigZag(node, false)));
+      longestZigZag(node.left, result);
+      longestZigZag(node.right, result);
+    }
+  }
+
+  /**
+   * 直接递归遍历
+   *
+   * @param node
+   * @param direction node节点的方向true为left
+   * @return
+   */
+  private int longestZigZag(TreeNode node, boolean direction) {
+    if (direction) {
+      if (node.left == null) {
+        return 0;
+      } else {
+        return 1 + longestZigZag(node.left, false);
+      }
+    } else {
+      if (node.right == null) {
+        return 0;
+      } else {
+        return 1 + longestZigZag(node.right, true);
+      }
+    }
+  }
+
+  /**
+   * 尾递归
+   *
+   * @param node
+   * @param direction 已node节点结尾，且是父节点的left（true）、right（false）：这样子节点可以沿用父子节的值
+   * @return
+   */
+  private void longestZigZagH(TreeNode node, boolean direction, int len, int[] result) {
+    if (node != null) {
+      result[0] = Math.max(result[0], len);
+      if (direction) {
+        if (node.left != null) {
+          longestZigZagH(node.left, true, 1, result);
+        }
+        if (node.right != null) {
+          longestZigZagH(node.right, false, len + 1, result);
+        }
+      } else {
+        if (node.left != null) {
+          longestZigZagH(node.left, true, len + 1, result);
+        }
+        if (node.right != null) {
+          longestZigZagH(node.right, false, 1, result);
+        }
+      }
     }
   }
 }
