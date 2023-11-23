294c306621cb1b786f6ee177d93e360cddcdeeba
zhenghui <15270855286@163.com>
2023-11-23 20:42:01 +0800

    20231123

---
+++ src/main/java/com/hz/arithmetic/lettcode/test100/Solution56.java
@@ -0,0 +1,36 @@
+package com.hz.arithmetic.lettcode.test100;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 合并区间
+ *
+ * @author zhenghui
+ * @since 2023/11/23
+ */
+public class Solution56 {
+
+  public int[][] merge(int[][] intervals) {
+    Arrays.sort(intervals, new Comparator<int[]>() {
+      @Override
+      public int compare(int[] o1, int[] o2) {
+        return o1[0] - o2[0];
+      }
+    });
+    List<int[]> merged = new ArrayList<>();
+    int len = intervals.length;
+    for (int[] interval : intervals) {
+      int l = interval[0], r = interval[1];
+      if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < l) {
+        merged.add(new int[]{l, r});
+      } else {
+        merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], r);
+      }
+    }
+    return merged.toArray(new int[merged.size()][]);
+  }
+}
